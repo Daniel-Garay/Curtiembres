dc0ba059f17539f08a3d80bef1021e5b4fd19304
Daniel Camilo Garay Cano <xcbdgara@intcomex.com>
2019-05-16 14:30:00 -0500

    cambio de controlador para direcionar

--- Curtiembres/src/java/edu/navegacion/controlador/NavegacionRequest.java
+++ Curtiembres/src/java/edu/navegacion/controlador/NavegacionRequest.java
@@ -1,4 +1,3 @@
-
 package edu.navegacion.controlador;
 
 import javax.inject.Named;
@@ -8,8 +7,20 @@ import javax.enterprise.context.Dependent;
 @Dependent
 public class NavegacionRequest {
 
-    
     public NavegacionRequest() {
     }
-    
+
+    private String redireccionTrue = "?faces-redirect=true";
+
+    public String moverPagIndex() {
+        return "index.xhtml" + redireccionTrue;
+    }
+
+    public String moverPagNosotros() {
+        return "nosotros.xhtml" + redireccionTrue;
+    }
+
+    public String moverPagRegistroUsuario() {
+        return "registroUsuario.xhtml" + redireccionTrue;
+    }
 }
